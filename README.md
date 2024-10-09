48b2d50da047a66021271ce224a224213700a25e
PiusLim373 <piuslim373@gmail.com>
2024-10-09 14:55:53 +0800

    Updated documentation and added demo script

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # drone_control
-This is a project for NUS MSc in Robotics module ME5418: Machine Learning in Robotics
+This is a machine learning based drone controlling project for NUS MSc in Robotics module ME5418: Machine Learning in Robotics.
+
+## Setup
+### 1. Clone the repo
+```
+git clone https://github.com/PiusLim373/drone_control.git
+```
+### 2. Setup using Conda
+```
+conda env create -f requirement.yml
+conda activate me5418-drone-control
+```
+This will create a conda environment me5418-drone-control with necessary packages installed to run the project.
+
+## Running the Gym Environment Demo (Assignment 1)
+Assignment 1 is about showing a working version of the gym environment that will allow subsequent program to call and step through the environment with specific action and get some state data in return.
+``` 
+python gym_demo.py
+```
+This demo script will create an environment in demo mode and run for 140steps for visualization. There are 4 checkpoints in this demo.
+
+:warning: You will need to press ECS key to end the rendering at the end of each checkpoint so that the demo will continue.
+
+#### Checkpoint 1
+First 25steps, spawn the quadcopter and wait for it to reach a stable stationary state.
+![](asset/docs/drone_stationary.png)
+
+#### Checkpoint 2
+For the next 50steps, activate all 4 rotors and the quadcopter will take off.
+![](asset/docs/drone_tookoff.png)
+
+#### Checkpoint 3
+For the next 50steps, activate the diagonal rotor 1 and rotor 3 and the quadcopter will rotate perform yaw rotation.
+![](asset/docs/drone_yaw_rotation.png)
+
+#### Checkpoint 4
+For the next 15steps, activate the back rotor 3 and rotor 4 and the quadcopter will rotate perform pitch rotation.
+![](asset/docs/drone_pitch_rotation.png)
+
+## Running the Neural Network Demo (Assignment 2) 
+Stay tuned, coming soon!
+## Running the Learning Agent Demo (Assignment 3) 
+Stay tuned, coming soon!
---
+++ gym_demo.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+from drone_control_gym import *
+
+# Create an instance of the DroneControlGym environment
+gym_env = DroneControlGym()
+print(f"step: {gym_env.step_count}")  # the gym environment starts with step 1
+
+# Run the simulation for 25 steps to allow the quadcopter to reach a stationary state
+while gym_env.step_count < 25:
+    print(f"step: {gym_env.step_count}, state: {gym_env.step(ACTIONS[0])}")
+# at the end of step 25, the quadcopter is at a stationary state and near position 0, 0, 0
+print(f"Drone pose at step {gym_env.step_count}: {gym_env.get_pose()}")
+# render the simulation, press Esc to continue
+gym_env.render()
+
+# Now run the simulation for 50 more steps with action that turns on all 4 rotors
+while gym_env.step_count < 75:
+    print(f"step: {gym_env.step_count}, state: {gym_env.step(ACTIONS[15])}")
+# at the end of step 75, the quadcopter has moved to a new position
+print(f"Drone pose at step {gym_env.step_count}: {gym_env.get_pose()}")
+# render the simulation, press Esc to continue
+gym_env.render()
+
+# Now run the simulation for 50 more steps with action that turns on the diagonal rotors (to rotate clockwise)
+while gym_env.step_count < 125:
+    print(f"step: {gym_env.step_count}, state: {gym_env.step(ACTIONS[5])}")
+# at the end of step 125, the quadcopter has rotated  pitch angle is changed
+print(f"Drone pose at step {gym_env.step_count}: {gym_env.get_pose()}")
+# render the simulation, press Esc to continue
+gym_env.render()
+
+# Now run the simulation for 15 more steps with action that turns on the back rotors (to rotate forward)
+while gym_env.step_count < 140:
+    print(f"step: {gym_env.step_count}, state: {gym_env.step(ACTIONS[3])}")
+# at the end of step 140, the quadcopter has rotated forward, pitch angle is changed
+print(f"Drone pose at step {gym_env.step_count}: {gym_env.get_pose()}")
+# render the simulation, press Esc to continue
+gym_env.render()
