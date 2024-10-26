100bc49375e5818332bb991726d6a092e1047863
AnsonCheah <ansoncta.sg@gmail.com>
2024-10-26 22:05:56 +0800

    rework reward structure, add drone position visualization

--- drone_control_gym.py
+++ drone_control_gym.py
@@ -11,30 +11,36 @@ from custom_utils import *
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 np.set_printoptions(suppress=True, threshold=np.inf, linewidth=np.inf)
 # Global variables
+XY_RANGE = 0.2
+Z_RANGE = 0.7
 RANGE_LIMIT = 1
 RANGE_BUFFER = 1
 RESOLUTION = 1
-GOAL_TOLERANCE = 0.1  # if the drone is within 0.1m of the goal, it is considered to have reached the goal
-FULL_THROTTLE = 7
+GOAL_TOLERANCE = 0.3  # if the drone is within 0.5m of the goal, it is considered to have reached the goal
+FULL_THROTTLE = 6
 DRONE_MODEL_PATH = os.path.join(os.getcwd(), "asset/skydio_x2/scene.xml")
 ROLL_TARGET = 5  # degrees
 PITCH_TARGET = 5  # degrees
 ROLL_THRESHOLD = 90  # degrees
 PITCH_THRESHOLD = 90  # degrees
-HEIGHT_LOWER_LIMIT = 0.05  # m
-IDLE_POSITION_THRESHOLD = 0.05  # m
+HEIGHT_LOWER_LIMIT = 0.2  # m
+IDLE_POSITION_THRESHOLD = 0.0001  # m
 GYRO_SMOOTH_THRESHOLD = 0.05  # Threshold for angular velocity (rad/s)
 ACC_SMOOTH_THRESHOLD = 0.1  # Threshold for linear acceleration (m/s^2)
-APPROACH_MULTIPLIER = 3
 
-(
-    ACTION_COST,
-    IDLE_COST,
-    GOAL_REWARD,
-    OUT_OF_BOUND_REWARD,
-    FLIPPED_REWARD,
-    SMOOTH_MOTION_REWARD,
-) = (-1, -10, 3000, -3000, -3000, 1)
+TILT_THRESHOLD = 30  # degrees
+MAX_TIMESTEPS = 300
+MAX_IDLE_STEP = 50
+GOAL_REWARD = 20
+MAX_APPROACH_REWARD = 5
+AWAY_MULTIPLIER = 1
+IDLE_PENALTY = -3
+FLIPPED_PENALTY = -2000
+GROUND_PENALTY = -20
+GOAL_ZONE_MULTIPLIER = 1000  # Scaling factor within goal zone
+APPROACH_MULTIPLIER = 500  # Scaling factor for distance reward
+TILT_PENALTY_MULTIPLER = 1  # Penalty scaling for tilt (flip avoidance)
+
 ACTIONS = [
     [0, 0, 0, 0],
     [0, 0, 0, 1],
@@ -65,10 +71,13 @@ class DroneControlGym(gym.Env):
         self.gyro_index = self.model.sensor_adr[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "body_gyro")]
         self.acc_index = self.model.sensor_adr[mujoco.mj_name2id(self.model, mujoco.mjtObj.mjOBJ_SENSOR, "body_linacc")]
         self.goal_id = self.model.geom("goal").id  # Get the index of the point geom
+        self.drone_id = self.model.geom("drone").id  # Get the index of the point geom
         self.renderflag = render
         if self.renderflag:
             self.viewer = mujoco_viewer.MujocoViewer(self.model, self.drone)
 
+        self.success_count = 0
+        self.time_in_goal = 0
         self.drone_acc = []
         self.drone_gyro = []
         self.drone_motor_thrust = None
@@ -87,18 +96,19 @@ class DroneControlGym(gym.Env):
         # randomly initialize goal pose (x, y, z) within RANGE_LIMIT
         if goal_pose == None:
             self.goal_pose = [
-                random.uniform(-RANGE_LIMIT / 2, RANGE_LIMIT / 2),
-                random.uniform(-RANGE_LIMIT / 2, RANGE_LIMIT / 2),
-                random.uniform(0.3, RANGE_LIMIT),
+                random.uniform(-XY_RANGE / 2, XY_RANGE / 2),
+                random.uniform(-XY_RANGE / 2, XY_RANGE / 2),
+                random.uniform(0.3, Z_RANGE),
             ]
         else:
             self.goal_pose = goal_pose
-
         self.drone.geom_xpos[self.goal_id] = np.copy(self.goal_pose)  # Set the position of the point
         self.distance_to_goal = None
         self.last_distance_to_goal = None
+        self.drone_angle_from_goal = None
+        self.last_drone_angle_from_goal = None
 
-        self.reward_counters = {"idle": 0, "action": 0, "approach": 0, "away": 0}
+        self.reward_counters = {"goal": 0, "approach": 0, "away": 0, "idle": 0, "ground": 0, "tilt": 0}
 
         self.has_finished = False
         mujoco.mj_step(self.model, self.drone)
@@ -126,128 +136,114 @@ class DroneControlGym(gym.Env):
             self.drone_rpy = [0.0, 0.0, 0.0]  # Set to [0, 0, 0] if not initialized
 
         # Calculate vector difference between goal and drone's current position
-        vector_to_goal_world = np.array(self.goal_pose) - np.array(self.drone_position)
+        goal_vector_world = np.array(self.goal_pose) - np.array(self.drone_position)
         drone_rot_mat = rpy_to_mat(self.drone_rpy)
 
+        # calculate goal unit vector in world frame
+        goal_unit_vector_world = goal_vector_world / np.linalg.norm(goal_vector_world)
+
+        # Get drone's Z-axis in world coordinates from its orientation
+        # Assuming drone_orientation is a rotation matrix (3x3)
+        drone_z_axis = drone_rot_mat[:, 2]  # Z-axis is the third column of the rotation matrix
+
+        # Calculate the dot product between the drone's Z-axis and the unit goal vector
+        # Ensure dot_product is between -1 and 1 (should be if normalized)
+        dot_product = np.clip(np.dot(drone_z_axis, goal_unit_vector_world), -1.0, 1.0)
+
+        # Calculate the angle between the two vectors in radians
+        self.drone_angle_from_goal = np.arccos(dot_product)
+
         # Transform the goal vector from the world frame to the drone's frame
-        vector_to_goal_drone_frame = np.dot(
-            np.linalg.inv(drone_rot_mat), vector_to_goal_world
-        )  # Inverse R because we want world to drone frame
+        # Inverse R because we want world to drone frame
+        goal_vector_drone_frame = np.dot(np.linalg.inv(drone_rot_mat), goal_vector_world)
 
         # Calculate distance to goal in the drone's frame
-        self.distance_to_goal = np.linalg.norm(vector_to_goal_drone_frame)
+        self.distance_to_goal = np.linalg.norm(goal_vector_drone_frame)
 
         # Normalize the vector to goal if the distance is greater than 0
         if self.distance_to_goal > 0:
-            dx, dy, dz = vector_to_goal_drone_frame / self.distance_to_goal  # Normalize
+            dx, dy, dz = goal_vector_drone_frame / self.distance_to_goal  # Normalize
         else:
             dx, dy, dz = 0.0, 0.0, 0.0  # If already at the goal
         return np.array([dx, dy, dz, self.distance_to_goal])
-    
-    
-    def _rpy_to_rotation_matrix(self, roll, pitch, yaw):
-        # Convert roll, pitch, yaw to rotation matrix
-        roll, pitch, yaw = np.radians([roll, pitch, yaw])
-
-        R_x = np.array([[1, 0, 0],
-                        [0, np.cos(roll), -np.sin(roll)],
-                        [0, np.sin(roll), np.cos(roll)]])
-        
-        R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-                        [0, 1, 0],
-                        [-np.sin(pitch), 0, np.cos(pitch)]])
-        
-        R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-                        [np.sin(yaw), np.cos(yaw), 0],
-                        [0, 0, 1]])
-        
-        # The final rotation matrix is R = R_z * R_y * R_x
-        R = np.dot(R_z, np.dot(R_y, R_x))
-        
-        return R
-
-
-    def _rpy_to_rotation_matrix(self, roll, pitch, yaw):
-        # Convert roll, pitch, yaw to rotation matrix
-        roll, pitch, yaw = np.radians([roll, pitch, yaw])
-
-        R_x = np.array([[1, 0, 0],
-                        [0, np.cos(roll), -np.sin(roll)],
-                        [0, np.sin(roll), np.cos(roll)]])
-        
-        R_y = np.array([[np.cos(pitch), 0, np.sin(pitch)],
-                        [0, 1, 0],
-                        [-np.sin(pitch), 0, np.cos(pitch)]])
-        
-        R_z = np.array([[np.cos(yaw), -np.sin(yaw), 0],
-                        [np.sin(yaw), np.cos(yaw), 0],
-                        [0, 0, 1]])
-        
-        # The final rotation matrix is R = R_z * R_y * R_x
-        R = np.dot(R_z, np.dot(R_y, R_x))
-        
-        return R
 
     def _calculate_reward(self):
+        global XY_RANGE, RANGE_LIMIT, Z_RANGE
         # calculate reward based on the current state of the drone and if the drone has reached the goal
         # Check if the roll and pitch are close to zero (upright)
-        if self.distance_to_goal < GOAL_TOLERANCE:
-            logging.info("The drone is staying upright at the goal.")
-            return True, GOAL_REWARD
-        else:
-            reward = 0
-            finished = False
+        reward = 0
+        finished = False
 
-            # check if current duty cycle is equal to previous duty cycle and position is same
-            if np.all(np.abs(self.drone_position - self.last_drone_position) < IDLE_POSITION_THRESHOLD):
-                logging.debug("drone is idle in place")
-                reward += IDLE_COST
-                self.reward_counters["idle"] += 1
+        if np.all(self.last_distance_to_goal):
+            # only counter here, not for reward
+            if self.last_distance_to_goal > self.distance_to_goal:
+                self.reward_counters["approach"] += 1
             else:
-                logging.debug("drone took valid action")
-                reward += ACTION_COST
-                self.reward_counters["action"] += 1
+                self.reward_counters["away"] += 1
 
-                # if drone is flipped (or collided?), return FLIPPED_REWARD
-                # REWARD = ACTION + FLIPPED
-                if abs(self.drone_rpy[0]) > ROLL_THRESHOLD or abs(self.drone_rpy[1]) > PITCH_THRESHOLD:
-                    logging.info("drone has flipped")
-                    reward += FLIPPED_REWARD
-                    finished = True
+            # When drone is within goal zone
+            if self.distance_to_goal < GOAL_TOLERANCE:
+                self.time_in_goal += 1
+                self.reward_counters["goal"] += 1
+                # flat reward for being in goal
+                reward += GOAL_REWARD
+                # scale reward for staying in goal
+                reward += 5 * (self.time_in_goal)
+                # scale reward for closer to center of goal
+                reward += GOAL_ZONE_MULTIPLIER * (self.last_distance_to_goal - self.distance_to_goal)
 
-                # if drone is flipped (or collided?), return FLIPPED_REWARD
-                # REWARD = ACTION + OUT
-                # if (
-                #     abs(self.drone_position[0]) >= (RANGE_LIMIT / 2 + RANGE_BUFFER)
-                #     or abs(self.drone_position[1]) >= (RANGE_LIMIT / 2 + RANGE_BUFFER)
-                #     or abs(self.drone_position[2]) >= (RANGE_LIMIT + RANGE_BUFFER)
-                # ):
-                #     logging.info("drone is out of bound")
-                #     reward += OUT_OF_BOUND_REWARD
-                #     finished = True
+                # level up challenges after being in goal for certain time steps
+                if self.time_in_goal >= 50:
+                    self.success_count += 1
+                    XY_RANGE *= 1.02  # XY range increases
+                    Z_RANGE *= 1.02  # Z range increases
+                    GOAL_TOLERANCE * 0.98  # Goal zone get smaller
+                    self.goal_pose = [
+                        random.uniform(-XY_RANGE / 2, XY_RANGE / 2),
+                        random.uniform(-XY_RANGE / 2, XY_RANGE / 2),
+                        random.uniform(0.2, Z_RANGE),
+                    ]
+                    print(
+                        f"LEVEL {self.success_count}: XY_RANGE: {XY_RANGE}, Z_RANGE: {Z_RANGE}, GOAL_TOLERANCE: {GOAL_TOLERANCE}"
+                    )
+                    finished = True  # Mission completed
+            else:
+                # positive reward for moving toward goal, negative reward for away
+                reward += APPROACH_MULTIPLIER * (self.last_distance_to_goal - self.distance_to_goal)
 
-                # Apply smooth motion rewards only if not flipped or out of bounds
-                if not finished:
-                    # Check for smooth angular motion (low angular velocity)
-                    # REWARD = ACTION + SMOOTH
-                    if self.distance_to_goal and self.last_distance_to_goal:
-                        if self.distance_to_goal < self.last_distance_to_goal:
-                            logging.debug("drone is approaching goal")
-                            reward += APPROACH_MULTIPLIER / (self.distance_to_goal + 0.003)
-                            self.reward_counters["approach"] += 1
-                        elif self.distance_to_goal >= self.last_distance_to_goal:
-                            logging.debug("drone is getting further from goal")
-                            reward -= APPROACH_MULTIPLIER
-                            self.reward_counters["away"] += 1
-                        # if np.all(np.abs(self.drone_gyro) < GYRO_SMOOTH_THRESHOLD):
-                        #     logging.debug("drone is rotating smoothly")
-                        #     reward += SMOOTH_MOTION_REWARD
-                        # # Check for smooth linear motion (low acceleration)
-                        # if np.all(np.abs(self.drone_acc) < ACC_SMOOTH_THRESHOLD):
-                        #     logging.debug("drone is moving smoothly")
-                        #     reward += SMOOTH_MOTION_REWARD
+                # penalty for being idle unless in goal
+                if np.all(np.abs(self.drone_position - self.last_drone_position) < IDLE_POSITION_THRESHOLD):
+                    logging.debug("Drone is idle.")
+                    self.reward_counters["idle"] += 1
+                    reward += (self.reward_counters["idle"]) * IDLE_PENALTY
+                # if self.drone_angle_from_goal < self.last_drone_angle_from_goal:
+                #     # Cap the alignment reward
+                #     logging.debug("Drone is aligning towards the goal.")
+                #     reward += 0.5 * (self.drone_angle_from_goal)
+                #     self.reward_counters["aligned"] += 1
 
-            return finished, reward
+        if np.all(self.drone_rpy):
+            if abs(self.drone_rpy[0]) > TILT_THRESHOLD or abs(self.drone_rpy[1]) > TILT_THRESHOLD:
+                # Excess tilt penalty
+                self.reward_counters["tilt"] += 1
+                reward += -TILT_PENALTY_MULTIPLER * (abs(self.drone_rpy[0]) + abs(self.drone_rpy[1]))
+
+        if self.drone_position[2] < HEIGHT_LOWER_LIMIT:
+            # penalty for being too near to ground
+            self.reward_counters["ground"] += 1
+            reward += GROUND_PENALTY
+
+        if self.step_count >= MAX_TIMESTEPS:
+            logging.info("Timed out")
+            finished = True
+
+        # Case 5: Check for flipping or collision
+        if abs(self.drone_rpy[0]) > ROLL_THRESHOLD or abs(self.drone_rpy[1]) > PITCH_THRESHOLD:
+            logging.info("Drone has flipped.")
+            reward += FLIPPED_PENALTY  # Severe penalty for flipping
+            finished = True
+
+        return finished, reward
 
     def get_pose(self):
         # Returns the current pose of the drone (x, y, z, roll, pitch, yaw)
@@ -281,31 +277,24 @@ class DroneControlGym(gym.Env):
         }
 
     def get_all_state(self):
-        combined_array = np.concatenate(
-            (self.goal_attributes, self.drone_rpy, self.drone_motor_thrust, self.drone_acc, self.drone_gyro)
-        )
+        # combined_array = np.concatenate(
+        #     (self.goal_attributes, self.drone_rpy, self.drone_motor_thrust, self.drone_acc, self.drone_gyro)
+        # )
+        combined_array = np.concatenate((self.goal_attributes, self.drone_rpy, self.drone_motor_thrust))
         observations = combined_array.tolist()
-        return (
-            self.reward,
-            self.has_finished,
-            observations,
-        )
+        return (self.reward, self.has_finished, observations)
 
     def reset(self, goal_pose=None):
         # Reset simulation data
         mujoco.mj_resetData(self.model, self.drone)
         # Randomly initialize a new goal pose
         if goal_pose == None:
-            self.goal_pose = [
-                random.uniform(-RANGE_LIMIT / 2, RANGE_LIMIT / 2),
-                random.uniform(-RANGE_LIMIT / 2, RANGE_LIMIT / 2),
-                random.uniform(0.3, RANGE_LIMIT),
-            ]
+            pass
         else:
             self.goal_pose = goal_pose
         self.drone.geom_xpos[self.goal_id] = np.copy(self.goal_pose)
         self.goal_attributes = self._calculate_goal_attributes()
-
+        self.time_in_goal = 0
         # Reset motor states
         self.motor_states = [
             [0] * RESOLUTION,
@@ -322,6 +311,8 @@ class DroneControlGym(gym.Env):
         self.drone_rpy = None
         self.distance_to_goal = None
         self.last_distance_to_goal = None
+        self.drone_angle_from_goal = None
+        self.last_drone_angle_from_goal = None
         self.drone_last_ctrl = np.zeros(4)
         self.step_count = 1
         self.has_finished = False
@@ -330,12 +321,10 @@ class DroneControlGym(gym.Env):
         self._update_drone_data_from_sim()
         self.drone_motor_thrust = np.array(self.drone.actuator_force)
 
-        self.reward_counters = {"idle": 0, "action": 0, "approach": 0, "away": 0}
+        self.reward_counters = {"goal": 0, "approach": 0, "away": 0, "idle": 0, "ground": 0, "tilt": 0}
 
         # Return the initial observation and goal
-        combined_array = np.concatenate(
-            (self.goal_attributes, self.drone_rpy, self.drone_motor_thrust, self.drone_acc, self.drone_gyro)
-        )
+        combined_array = np.concatenate((self.goal_attributes, self.drone_rpy, self.drone_motor_thrust))
         observations = combined_array.tolist()
         return observations
 
@@ -347,6 +336,8 @@ class DroneControlGym(gym.Env):
             f"                                                           [{action[3]}] [{action[2]}]"
         )
         self.last_distance_to_goal = self.distance_to_goal
+        self.last_drone_angle_from_goal = self.drone_angle_from_goal
+        self.last_drone_position = np.copy(self.drone_position)
         # using action given, pop the first motor state and append the new motor state for each motor
         for index, individual_action in enumerate(action):
             self.motor_states[index].pop(0)
@@ -357,6 +348,7 @@ class DroneControlGym(gym.Env):
         # step through the simulator and get data
         mujoco.mj_step(self.model, self.drone)
         if self.renderflag:
+            self.drone.geom_xpos[self.drone_id] = np.copy(self.drone_position)
             self.drone.geom_xpos[self.goal_id] = np.copy(self.goal_pose)  # Set the position of the point
             self.viewer.render()
         self.step_count += 1
@@ -381,4 +373,6 @@ class DroneControlGym(gym.Env):
         # Render for visualization, press Esc to continue
         self.viewer = mujoco_viewer.MujocoViewer(self.model, self.drone)
         while self.viewer.is_alive:
+            self.drone.geom_xpos[self.goal_id] = np.copy(self.goal_pose)
+            self.drone.geom_xpos[self.drone_id] = np.copy(self.drone_position)
             self.viewer.render()
